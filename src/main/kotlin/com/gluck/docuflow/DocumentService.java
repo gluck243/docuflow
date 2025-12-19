71eac864f8ef4e47174116a6226e6a10ad6a41b0
gluck243 <gluck243@gmail.com>
2025-12-19 19:30:07 +0200

    feat(service): add llm pdf file processing
    Updated service to process pdf files by chunking using all-minilm by ollama and storing data in vectore store

--- src/main/kotlin/com/gluck/docuflow/DocumentService.java
+++ src/main/kotlin/com/gluck/docuflow/DocumentService.java
@@ -1,19 +1,27 @@
 package com.gluck.docuflow;
 
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class DocumentService {
 
     private final DocumentRepository repository;
+    private final VectorStore vectorStore;
 
-    public DocumentService(DocumentRepository repository) {
+    public DocumentService(DocumentRepository repository, VectorStore vectorStore) {
         this.repository = repository;
+        this.vectorStore = vectorStore;
     }
 
     public DocumentDTO upload(MultipartFile file) {
@@ -28,6 +36,24 @@ public class DocumentService {
         doc.setSize(file.getSize());
         doc.setUploadDate(LocalDateTime.now());
         Document saved = repository.save(doc);
+
+        try {
+            var pdfResource = new ByteArrayResource(file.getBytes());
+            var reader = new PagePdfDocumentReader(pdfResource);
+            List<org.springframework.ai.document.Document> rawChunks = reader.get();
+            List<org.springframework.ai.document.Document> cleanChunks = rawChunks.stream()
+                    .filter(chunk -> chunk.getContent() != null && !chunk.getContent().isBlank())
+                    .map(chunk -> new org.springframework.ai.document.Document(chunk.getContent(), Map.of()))
+                    .toList();
+            var splitter = new TokenTextSplitter(300, 30, 5, 10000, true);
+            List<org.springframework.ai.document.Document> splitChunks = splitter.apply(cleanChunks);
+
+            // Store in Vector DB
+            vectorStore.add(splitChunks);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to process AI embedding", e);
+        }
+
         return new DocumentDTO(saved.getId(), saved.getFilename(), saved.getSize(), saved.getUploadDate());
     }
 
